f9734fc74a78b5b7201b9db054a1a699b14541a4
zeeshanabhatti <156796090+zeeshanabhatti@users.noreply.github.com>
2024-09-26 20:31:34 +0500

    Update login.js

--- login.js
+++ login.js
@@ -6,3 +6,36 @@ var users = [
   { username: 'student', password: '2024', expiryDate: new Date(2024, 12, 30) },
   { username: 'guest', password: '1234', expiryDate: new Date(2099, 12, 30) }
 ];
+
+var excludedPages = [
+  'https://zeepaktech.blogspot.com/',
+  'https://zeepaktech.blogspot.com/?m=1',
+  'https://zeepaktech.blogspot.com/p/alert.html',
+  'https://zeepaktech.blogspot.com/p/alert.html/?m=1'
+];
+
+if (!excludedPages.includes(window.location.href)) {
+  // Check if the username and password are stored in cookies
+  var un = document.cookie.replace(/(?:(?:^|.*;\s*)username\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+  var pd = document.cookie.replace(/(?:(?:^|.*;\s*)password\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+
+  if (!un || !pd) {
+    un = prompt('Enter your User Name:', '');
+    pd = prompt('Enter your Password:', '');
+
+    // Check if the entered username and password match any of the allowed users
+    if (!users.some(user => user.username === un && user.password === pd)) {
+      location.href = 'https://zeepaktech.blogspot.com'; // Redirect if not found
+    } else {
+      // Check if the password has expired for the entered username
+      if (users.find(user => user.username === un).expiryDate <= new Date()) {
+        alert('Your account has been expired. Please, renew your User Name and Password.');
+        location.href = 'zeepaktech.blogspot.com'; // Redirect if expired
+      } else {
+        // Store the username and password in cookies
+        document.cookie = "username=" + un;
+        document.cookie = "password=" + pd;
+      }
+    }
+  }
+}
