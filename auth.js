a4af4659cd204c2a5c26aaa15e65b8f6b3ddf1b8
zeeshanabhatti <156796090+zeeshanabhatti@users.noreply.github.com>
2024-09-28 10:08:27 +0500

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,8 @@
+// Create an array to store user objects with usernames, passwords, and expiry dates
+var users = [
+  { username: 'user1', password: '123123', expiryDate: new Date(2023, 2, 25) },
+  { username: 'zeeshan', password: 'unlockit', expiryDate: new Date(2024, 12, 30) },
+  { username: 'sonia', password: 'opennow', expiryDate: new Date(2024, 12, 30) },
+  { username: 'student', password: '2024', expiryDate: new Date(2024, 12, 30) },
+  { username: 'guest', password: '1234', expiryDate: new Date(2099, 12, 30) }
+];
