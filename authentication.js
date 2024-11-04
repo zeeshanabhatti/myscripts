32d1285a48a12a6f76ea0730edd1558ce73486ec
zeeshanabhatti <156796090+zeeshanabhatti@users.noreply.github.com>
2024-11-04 19:26:13 +0500

    Update authentication.js

--- authentication.js
+++ authentication.js
@@ -12,7 +12,7 @@
     }
 
     const users = [
-        { username: 'guest', password: '1234', expiry: new Date('2099-12-31') },
+        { username: 'guest', password: '1234', expiry: new Date('2023-12-31') },
         { username: 'user1', password: 'pass1', expiry: new Date('2024-12-31') },
         { username: 'user2', password: 'pass2', expiry: new Date('2024-10-01') },
         { username: 'user3', password: 'pass3', expiry: new Date('2023-01-01') }
