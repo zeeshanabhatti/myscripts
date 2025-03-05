2fc07c696c4812fe8f6eecb8edeb44e5f1af6520
Zeeshan Ahmad Bhatti <156796090+zeeshanabhatti@users.noreply.github.com>
2025-03-05 21:03:35 +0500

    Update pwds.js

--- pwds.js
+++ pwds.js
@@ -1,6 +1,6 @@
     // Define an array of passwords with unique IDs
     const passwords = [
       { id: 'book1', password: 'PredefinedPassword.document.getElementByIdbook1' },
-      { id: 'book2', password: '456456' },
-      { id: 'book3', password: '789789' }
+      { id: 'book2', password: 'PredefinedPassword.document.getElementByIdbook2' },
+      { id: 'book3', password: 'PredefinedPassword.document.getElementByIdbook3' }
     ];
