a483124c11fde94c8624cc0079f7a9edb7b259ed
Zeeshan Ahmad Bhatti <156796090+zeeshanabhatti@users.noreply.github.com>
2025-03-12 19:53:57 +0500

    Create pswdv.js

---
+++ pswdv.js
@@ -0,0 +1,6 @@
+                // Define an array of passwords with unique IDs
+                const passwords = [
+                    { id: 'video1', password: 'PredefinedPassword.document.getElementByIdbook1' },
+                    { id: 'video2', password: '456456' },
+                    { id: 'video3', password: '789789' }
+                ];
